3b0fb4ab53dfb0b4372913c57765b7173e3b22b6
Joe <josephhaenel@gmail.com>
2023-01-22 20:40:54 -0600

    Added heap sort

---
+++ CS340_Project1-master/README.txt
@@ -0,0 +1,21 @@
+* In the PermutedSortingTime1.png, merge sort and heap sort are both graphed, you can just barely see them because
+    of the scale of insertion sort and the similarity/speed of sorting time between heap sort and merge sort compared to insertion.
+
+* You may need to close the graphs after they display for the code to continue execution. But you definitely need to for the next graph to display. 
+
+How to run:
+
+	Option 1:
+		1. Go to file manager
+		2. Locate "CS340_Project1-master"
+		3. Open it and double click on main.py to open Python IDLE
+		4. Run program (F5)
+		(Takes a while to execute Insertion Sort...)
+
+	Option 2:
+		1. Open Linux Terminal
+		2. ls -l to see where you currently are
+		3. cd into folder "CS340_Project1-master" is located
+		4. cd into "CS340_Project1-master"
+		5. type "python -u main.py" to run the program
+
--- CS340_Project1-master/main.py
+++ CS340_Project1-master/main.py
@@ -8,8 +8,10 @@ import os
 infinity = 'zzzzzzzzzz'
 heapSize = 0
 
+numFiles = 10
 
-def insertionSort(A):  # Working
+
+def insertionSort(A): 
 
     for j in range(1, len(A)):
         key = A[j]
@@ -30,7 +32,7 @@ def mergeSort(Array, start,end):
         merge(Array, start, mid, end)
 
 
-def merge(A, p, q, r):  # Issue with sorting
+def merge(A, p, q, r): 
     n1 = q - p + 1
     n2 = r - q
 
@@ -52,104 +54,123 @@ def merge(A, p, q, r):  # Issue with sorting
             A[k] = R[j]
             j = j + 1
 
-# def buildMaxHeap(A):
-#     heapSize = len(A)
-#     for i in range(0, int(len(A) / 2)):
-#         maxHeapify(A, i)
 
-# def heapSort(A):
-#     buildMaxHeap(A)
-#     for i in range(0, len(A)):
-#         A[1] = A[i]
-#         heapSize = heapSize - 1
-#         maxHeapify(A, 1)
+def buildMaxHeap(A):
+    heapSize = len(A)
+    for i in range(int((len(A) / 2) - 1), -1, -1):
+        maxHeapify(A, i, heapSize)
 
-# def maxHeapify(A, i):
-#     l =
+
+def heapSort(A):
+    heapSize = len(A)
+    buildMaxHeap(A)
+    for i in range(len(A) - 1, 0, -1):
+        A[i], A[0] = A[0], A[i]
+        heapSize -= 1
+        maxHeapify(A, 0, heapSize)
+
+
+def maxHeapify(A, i, heapSize):
+    l = 2 * i + 1
+    r = 2 * i + 2
+    if l < heapSize and A[l] > A[i]:
+        largest = l
+    else:
+        largest = i
+    if r < heapSize and A[r] > A[largest]:
+        largest = r
+    if largest != i:
+        A[i], A[largest] = A[largest], A[i]
+        maxHeapify(A, largest, heapSize)
+
 
 # Main function
 
 def main():
-    for j in range(2):
-        if j == 0:
-            sortType = 'IS'
-        elif j == 1:
-            sortType = 'MS'
-        elif j == 2:
-            sortType = 'HS'
-        x1 = [0]
-        y1 = [0]
-        fileSize = 0
-        for i in range(3):
-            fileSize += 15
-            fileName = 'perm' + str(fileSize) + 'K.txt'
-
-            fileDestination = os.path.join(
-                *[os.path.dirname(__file__), 'wordlists', 'PERM', fileName])
-
-            try:
-
-                # Reading from Files
-
-                fileObject = open(fileDestination, "r")
-
-                word_list = fileObject.readlines()
-
-                fileObject.close()
-
-                start_time = time.time()
-                if j == 0:
-                    insertionSort(word_list)  # Meat and Potatoes
-                elif j == 1:
-                    n = len(word_list) - 1
-                    mergeSort(word_list, 0, n)
-                end_time = time.time()
-                x1.append(fileSize)
-                y1.append(end_time - start_time)
-
-                # Writing to Files
-
-                fileName = sortType + str(fileSize) + 'K.txt'
+    for t in range(2):
+        if t == 0:
+            sortList = "PERM"
+            sortFileType = "perm"
+            sortOutFile = 'PERM_OUT'
+        elif t == 1:
+            sortList = "SORTED"
+            sortFileType = "sorted"
+            sortOutFile = 'SORT_OUT'
+        for j in range(3):
+            if j == 0:
+                sortType = 'IS'
+            elif j == 1:
+                sortType = 'MS'
+            elif j == 2:
+                sortType = 'HS'
+            x1 = [0]
+            y1 = [0]
+            fileSize = 0
+            for i in range(numFiles):
+                fileSize += 15
+                fileName = sortFileType + str(fileSize) + 'K.txt'
 
                 fileDestination = os.path.join(
-                    *[os.path.dirname(__file__), 'wordlists', 'PERM_OUT', fileName])
+                    *[os.path.dirname(__file__), 'wordlists', sortList, fileName])
 
-                fileObject = open(fileDestination, 'w')
+                try:
 
-                for line in word_list:
-                    fileObject.writelines(line)
+                    # Reading from Files
 
-                fileObject.close()
+                    fileObject = open(fileDestination, "r")
 
-            except IOError:
-                print(fileName + ' failed to open')
+                    word_list = fileObject.readlines()
+
+                    fileObject.close()
+
+                    start_time = time.time()
+                    if j == 0:
+                        insertionSort(word_list)  # Meat and Potatoes
+                    elif j == 1:
+                        n = len(word_list) - 1
+                        mergeSort(word_list, 0, n)
+                    elif j == 2:
+                        heapSort(word_list)
+                    end_time = time.time()
+                    x1.append(fileSize)
+                    y1.append(end_time - start_time)
+
+                    # Writing to Files
+
+                    fileName = sortType + str(fileSize) + 'K.txt'
+
+                    fileDestination = os.path.join(
+                        *[os.path.dirname(__file__), 'wordlists', sortOutFile, fileName])
+
+                    fileObject = open(fileDestination, 'w')
+
+                    for line in word_list:
+                        fileObject.writelines(line)
+
+                    fileObject.close()
+
+                except IOError:
+                    print(fileName + ' failed to open')
+                    
+            if j == 0:
+                plt.plot(x1, y1, label="Insertion Sort")
+            elif j == 1:
+                plt.plot(x1, y1, label="Merge Sort")
+            elif j == 2:
+                plt.plot(x1, y1, label="Heap Sort")
                 
-        if j == 0:
-            plt.plot(x1, y1, label="Insertion Sort")
-        elif j == 1:
-            plt.plot(x1, y1, label="Merge Sort")
-        elif j == 2:
-            plt.plot(x1, y1, label="Heap Sort")
-            
-    plt.rcParams['figure.figsize'] = [10, 6]  # Setting size of plot
+        plt.rcParams['figure.figsize'] = [10, 6]  # Setting size of plot
+        plt.rcParams["figure.autolayout"] = True
 
-    # labels = np.array([15, 30, 45, 60, 75, 90, 100, 115, 130, 150])
-    # plt.xticks(x1, labels)
-    plt.xlabel('Words Sorted (thousands)')
-    plt.ylabel('Time Taken (seconds)')
-    plt.title("Sorting Time")
-    plt.legend()
-    plt.show()
+        plt.xlabel('Words Sorted (thousands)')
+        plt.ylabel('Time Taken (seconds)')
+        if t == 0:
+            plt.title("Permuted Sorting Time")
+        elif t == 1:
+            plt.title("Sorted Sorting Time")
+        plt.legend()
+        plt.show()
 
-main()
-
-
-# testSortingList = ['date', 'apple', 'banana', 'cucumber', 'acorn', 'aaaa']
-# n = len(testSortingList) - 1
-# mergeSort(testSortingList, 0, n)
-# print('Merge Sort:     ', testSortingList)
-
-# testSortingList = ['date', 'apple', 'banana', 'cucumber', 'acorn', 'aaaa']
-
-# insertionSort(testSortingList)
-# print('Insertion Sort: ', testSortingList)
+        
+if __name__ == '__main__':
+    main()
